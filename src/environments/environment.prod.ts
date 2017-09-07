855766ea0ebecd4ed47196e8c93de69a4296c521
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-09-07 22:27:05 +0200

    Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDhlks5fYTB1sky3O1hQ9xFwLDS6reszaE',
+    authDomain: 'tryout-fca6f.firebaseapp.com',
+    databaseURL: 'https://tryout-fca6f.firebaseio.com',
+    projectId: 'tryout-fca6f',
+    storageBucket: '',
+    messagingSenderId: '97687928687'
+  },
+  graphql: {
+    endpoint: 'https://pp13m5710.lp.gql.zone/graphql'
+  }
 };
